e188af868dcd253b9972a1955681dbdf0171c8c0
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-04-02 14:04:02 -0600

    Se agrega copia al hacer clic en el correo y enlace a github

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Supports weights 100-900
-import '@fontsource-variable/onest';
+console.log("hola mundo");
+
+function copyEmail() {
+    event.preventDefault();
+    const emailElement = document.getElementById("email-link");
+    const email = emailElement.childNodes[emailElement.childNodes.length - 1].nodeValue.trim();
+    navigator.clipboard.writeText(email).then(() => {
+        const message = document.getElementById("message");
+        message.style.display = "block";
+        setTimeout(() => {
+            message.style.display = "none"
+        }, 2000);
+    }).catch(err => console.error("Error al copiar: ", err)
+    );
+}
